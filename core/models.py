a0e98188253b46cd4f4b5aa413356e82595d6164
Biola <52200708+decorouz@users.noreply.github.com>
2024-06-07 21:47:59 +0100

    setup core app

--- core/models.py
+++ core/models.py
@@ -6,3 +6,14 @@ from django.db import models
 
 class User(AbstractUser):
     email = models.EmailField(verbose_name="email address", unique=True)
+
+
+class TimeStampedModel(models.Model):
+    """An abstract base class model that provides selfupdating
+    ``created`` and ``modified`` fields."""
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
