69b454946c1854840cf242d9a1ef214b6eabfa85
Biola <52200708+decorouz@users.noreply.github.com>
2024-06-10 00:41:40 +0100

    reorganize project setup

--- farmers/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("farmers/", views.index, name="index"),
-]
--- market/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    # path("farmers/", views.index, name="index"),
-]
