a7707d7cde95b1683fb6e885c504cc39cfcc7842
Biola <52200708+decorouz@users.noreply.github.com>
2024-06-28 21:46:09 +0100

    core app and database migration

--- src/core/models.py
+++ src/core/models.py
@@ -1,19 +1,23 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
 
 
-class User(AbstractUser):
-    email = models.EmailField(verbose_name="email address", unique=True)
-
-
 class TimeStampedModel(models.Model):
     """An abstract base class model that provides selfupdating
     ``created`` and ``modified`` fields."""
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
+
+
+class User(AbstractUser, TimeStampedModel):
+    email = models.EmailField(verbose_name="email address", unique=True)
