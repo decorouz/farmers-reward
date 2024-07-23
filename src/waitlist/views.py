abb647ac24733974a22aaa2b050661d321ba908f
Biola <52200708+decorouz@users.noreply.github.com>
2024-07-23 03:15:51 +0100

    apply migration to prod db

--- src/waitlist/views.py
+++ src/waitlist/views.py
@@ -1,5 +1,3 @@
-import logging
-
 from django.contrib import messages
 from django.shortcuts import redirect, render
 
@@ -7,26 +5,15 @@ from .forms import WaitlistForm
 
 # Create your views here.
 
-logger = logging.getLogger(__name__)
-
 
 def join_waitlist(request):
     if request.method == "POST":
         form = WaitlistForm(request.POST)
-        logger.info(f"Form data: {request.POST}")
         if form.is_valid():
-            logger.info("Form is valid")
-            try:
+            form.save()
+            messages.success(request, "You have successfully joined the waitlist.")
+            return redirect("waitlist_success")
 
-                instance = form.save()
-                logger.info(f"Instance saved: {instance}")
-                messages.success(request, "You have successfully joined the waitlist.")
-                return redirect("waitlist_success")
-            except Exception as e:
-                logger.error(f"Error saving form: {str(e)}")
-                messages.error(request, "An error occurred. Please try again.")
-        else:
-            logger.error(f"Form errors: {form.errors}")
     else:
         form = WaitlistForm()
     return render(request, "waitlist/join_waitlist_form.html", {"form": form})
