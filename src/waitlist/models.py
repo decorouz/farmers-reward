1ac4cfe7c870a81ea610b4bf69756932435d2b72
Biola <52200708+decorouz@users.noreply.github.com>
2024-10-01 23:37:29 +0100

    update model migrations

--- src/waitlist/models.py
+++ src/waitlist/models.py
@@ -4,10 +4,11 @@ from core.models import BaseModel
 
 
 # Create your models here.
-class Contact(BaseModel):
+class Contact(models.Model):
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=100)
     message = models.TextField(blank=False, null=False)
+    created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.name}-{self.email}"
