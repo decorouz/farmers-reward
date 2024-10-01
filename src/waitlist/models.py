a56900281ff36c1a597c4e188b2dc4287ea077d9
Biola <52200708+decorouz@users.noreply.github.com>
2024-10-01 23:55:17 +0100

    fix waitlist updated at

--- src/waitlist/models.py
+++ src/waitlist/models.py
@@ -8,7 +8,8 @@ class Contact(models.Model):
     name = models.CharField(max_length=100)
     phone_number = PhoneNumberField(unique=True)
     message = models.TextField(blank=False, null=False)
-    created_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.name}-{self.email}"
